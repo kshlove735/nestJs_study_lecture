3f889fe2a3996ba6d784c2884c0613e2717178a0
kshlove735 <kshlove735@gmail.com>
2022-06-15 18:11:51 +0900

    Swagger API 보안설정 & 로그인API 프론트엔드와 연결

--- 03 프로젝트 1 - 고양이 정보 커뮤니티/project/src/main.ts
+++ 03 프로젝트 1 - 고양이 정보 커뮤니티/project/src/main.ts
@@ -1,20 +1,29 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { title } from 'process';
+import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
+import * as expressBasicAuth from 'express-basic-auth';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe()); // schema에서 Class validation 쓰기위해서
+  app.useGlobalPipes(new ValidationPipe({ transform: true })); // schema에서 Class validation 쓰기위해서
   app.useGlobalFilters(new HttpExceptionFilter()); //nest에서 제공하는 오류 filter-> 전역 설정
+  app.use(
+    ['/docs', '/docs-json'],
+    expressBasicAuth({
+      challenge: true,
+      users: {
+        [process.env.SWAGGER_USER]: process.env.SWAGGER_PASSWORD,
+      },
+    }),
+  );
   const config = new DocumentBuilder()
     .setTitle('C.I.C')
     .setDescription('cat')
     .setVersion('1.0.0')
     .build();
-  const document = SwaggerModule.createDocument(app, config);
+  const document: OpenAPIObject = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
   app.enableCors({
     // 개발중 : true, 배포: 특정 url
