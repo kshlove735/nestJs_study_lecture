26650f1003e70fa64cf094003b4ee458739bad19
kshlove735 <kshlove735@gmail.com>
2022-06-16 17:44:53 +0900

    Multer와 미디어 파일 업로드 구현

--- 03 프로젝트 1 - 고양이 정보 커뮤니티/project/src/main.ts
+++ 03 프로젝트 1 - 고양이 정보 커뮤니티/project/src/main.ts
@@ -1,12 +1,14 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
 import * as expressBasicAuth from 'express-basic-auth';
+import * as path from 'path';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true })); // schema에서 Class validation 쓰기위해서
   app.useGlobalFilters(new HttpExceptionFilter()); //nest에서 제공하는 오류 filter-> 전역 설정
   app.use(
@@ -18,6 +20,12 @@ async function bootstrap() {
       },
     }),
   );
+
+  // 위에 app에 <NestExpressApplication> 처리해줘야 userStaticAssets 사용 가능
+  app.useStaticAssets(path.join(__dirname, './common', 'uploads'), {
+    prefix: '/media',
+  });
+
   const config = new DocumentBuilder()
     .setTitle('C.I.C')
     .setDescription('cat')
