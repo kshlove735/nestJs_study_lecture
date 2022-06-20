c98a56cc4941c3a009e8e7b28ebf3bf21720d14e
kshlove735 <kshlove735@gmail.com>
2022-06-20 11:35:33 +0900

    유니캐스팅: emit & on

--- 04 프로젝트 2-랜덤 채팅 웹 앱/chattings/public/js/scripts.js
+++ 04 프로젝트 2-랜덤 채팅 웹 앱/chattings/public/js/scripts.js
@@ -8,6 +8,14 @@ const formElement = getElementById('chat_form');
 
 function helloUser() {
   const username = prompt('What is your name?');
+  // 보내는 함수: 'new_user'라는 이벤트로 username 보냄
+  socket.emit('new_user', username, (data) => {
+    console.log(data);
+  });
+  console.log(username);
+  socket.on('hello_user', (data) => {
+    console.log(data);
+  });
 }
 
 function init() {
