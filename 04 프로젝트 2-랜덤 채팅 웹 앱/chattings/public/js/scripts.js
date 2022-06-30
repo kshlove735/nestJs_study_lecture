84ac3eaafa2cc8d7e93d297d398884a84871304e
kshlove735 <kshlove735@gmail.com>
2022-06-30 19:33:19 +0900

    브로드캐스팅(Broadcasting)

--- 04 프로젝트 2-랜덤 채팅 웹 앱/chattings/public/js/scripts.js
+++ 04 프로젝트 2-랜덤 채팅 웹 앱/chattings/public/js/scripts.js
@@ -6,15 +6,20 @@ const helloStringElement = getElementById('hello_stranger');
 const chattingBoxElement = getElementById('chatting_box');
 const formElement = getElementById('chat_form');
 
+// draw function
+const drawHelloStranger = (username) =>
+  (helloStringElement.innerHTML = `Hello ${username} Stranger :)`);
+
+// global socket handler
+socket.on('user_connected', (username) => {
+  console.log(`${username} connected!`);
+});
+
 function helloUser() {
   const username = prompt('What is your name?');
   // 보내는 함수: 'new_user'라는 이벤트로 username 보냄, 익명함수(서버에서 리턴한 데이터)
   socket.emit('new_user', username, (data) => {
-    console.log(data);
-  });
-  console.log(username);
-  socket.on('hello_user', (data) => {
-    console.log(data);
+    drawHelloStranger(data);
   });
 }
 
--- 04 프로젝트 2-랜덤 채팅 웹 앱/chattings/src/chats/chats.gateway.ts
+++ 04 프로젝트 2-랜덤 채팅 웹 앱/chattings/src/chats/chats.gateway.ts
@@ -10,6 +10,7 @@ import {
 } from '@nestjs/websockets';
 import { Socket } from 'socket.io';
 
+//namespace : @Controller()의 endpoint와 유사한 개념 sciprt의 const socket = io('/chattings');과 매칭
 @WebSocketGateway({ namespace: 'chattings' })
 export class ChatsGateway
   implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
@@ -19,25 +20,29 @@ export class ChatsGateway
   constructor() {
     this.logger.log('constructor');
   }
+
+  // client와 연결이 끊기면 실행
   handleDisconnect(@ConnectedSocket() socket: Socket) {
     this.logger.log(`disconnected: ${socket.id} ${socket.nsp.name}`);
   }
+
+  // client와 연결이 되자마자 실행
   handleConnection(@ConnectedSocket() socket: Socket) {
-    this.logger.log(`connected: ${socket.id} ${socket.nsp.name}`);
+    this.logger.log(`connected: ${socket.id} ${socket.nsp.name}`); // nsp.name -> namespace 이름
   }
 
+  // constructor 다음으로 제일 먼저 실행되는 메소드
   afterInit() {
     this.logger.log('init');
   }
 
   @SubscribeMessage('new_user')
   handleNewUser(
-    @MessageBody() username: string,
+    @MessageBody() username: string, // client한테 받은 메시지
     @ConnectedSocket() socket: Socket,
   ) {
-    console.log(socket.id);
-    console.log(username);
-    socket.emit('hello_user', 'hello ' + username);
-    return 'hello world';
+    // broadcast : 연결된 모든 소켓들에게 데이터 전송
+    socket.broadcast.emit('user_connected', username); // emit('이벤트 이름', 보내는 데이터);
+    return username;
   }
 }
